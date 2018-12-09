fb0c1ac4554461249ca96a64422e0bc57adee7d9
ia <isaac.ardis@gmail.com>
2018-12-09 09:37:58 -0600

    add COToken and COVerified tp fields

--- trackPoint/trackPoint.go
+++ trackPoint/trackPoint.go
@@ -8,21 +8,23 @@ import (
 
 // TrackPoint Stores a snippet of life, love, and location
 type TrackPoint struct {
-	Uuid      string    `json:"uuid"`
-	PushToken string    `json:"pushToken"`
-	Version   string    `json:"version"`
-	ID        int64     `json:"id"` //either bolt auto id or unixnano //think nano is better cuz can check for dupery
-	Name      string    `json:"name"`
-	Lat       float64   `json:"lat"`
-	Lng       float64   `json:"long"`
-	Accuracy  float64   `json:"accuracy"`  // horizontal, in meters
-	Elevation float64   `json:"elevation"` //in meters
-	Speed     float64   `json:"speed"`     //in kilometers per hour
-	Tilt      float64   `json:"tilt"`      //degrees?
-	Heading   float64   `json:"heading"`   //in degrees
-	HeartRate float64   `json:"heartrate"` // bpm
-	Time      time.Time `json:"time"`
-	Notes     string    `json:"notes"` //special events of the day
+	Uuid       string    `json:"uuid"`
+	PushToken  string    `json:"pushToken"`
+	Version    string    `json:"version"`
+	ID         int64     `json:"id"` //either bolt auto id or unixnano //think nano is better cuz can check for dupery
+	Name       string    `json:"name"`
+	Lat        float64   `json:"lat"`
+	Lng        float64   `json:"long"`
+	Accuracy   float64   `json:"accuracy"`  // horizontal, in meters
+	Elevation  float64   `json:"elevation"` //in meters
+	Speed      float64   `json:"speed"`     //in kilometers per hour
+	Tilt       float64   `json:"tilt"`      //degrees?
+	Heading    float64   `json:"heading"`   //in degrees
+	HeartRate  float64   `json:"heartrate"` // bpm
+	Time       time.Time `json:"time"`
+	Notes      string    `json:"notes"` //special events of the day
+	COToken    string    `json:"apiCOToken"`
+	COVerified bool      `json:"COVerified"`
 }
 
 // Vector deepens
