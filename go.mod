e16f66e92420079989c69557b71dbabdb1bf797a
ia <isaac.ardis@gmail.com>
2024-02-04 10:27:17 -0700

    add speed_accuracy and heading_accuracy fields
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/rotblauer/trackpoints
 go 1.17
 
 require (
-	github.com/deet/simpleline v0.0.0-20140919022041-9d297ff784a2
 	github.com/rotblauer/google-geolocate v0.0.0-20170109034844-ce12f6c55324
 	github.com/rotblauer/trackpoints/trackPoint v0.0.0-20220630170348-f4cc23102652
 )
 
-require github.com/stretchr/testify v1.8.0 // indirect
+require (
+	github.com/deet/simpleline v0.0.0-20140919022041-9d297ff784a2 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+)
--- trackPoint/trackPoint.go
+++ trackPoint/trackPoint.go
@@ -9,25 +9,31 @@ import (
 
 // TrackPoint Stores a snippet of life, love, and location
 type TrackPoint struct {
-	Uuid       string    `json:"uuid"`
-	PushToken  string    `json:"pushToken"`
-	Version    string    `json:"version"`
-	ID         int64     `json:"id"` //either bolt auto id or unixnano //think nano is better cuz can check for dupery
-	Name       string    `json:"name"`
-	Lat        float64   `json:"lat"`
-	Lng        float64   `json:"long"`
-	Accuracy   float64   `json:"accuracy"`  // horizontal, in meters
-	VAccuracy  float64   `json:"vAccuracy"` // vertical, in meteres
-	Elevation  float64   `json:"elevation"` //in meters
-	Speed      float64   `json:"speed"`     //in kilometers per hour
-	Tilt       float64   `json:"tilt"`      //degrees?
-	Heading    float64   `json:"heading"`   //in degrees
-	HeartRate  float64   `json:"heartrate"` // bpm
-	Time       time.Time `json:"time"`
-	Floor      int       `json:"floor"` // building floor if available
-	Notes      string    `json:"notes"` //special events of the day
-	COVerified bool      `json:"COVerified"`
-	RemoteAddr string    `json:"remoteaddr"`
+	Uuid            string    `json:"uuid"`
+	PushToken       string    `json:"pushToken"`
+	Version         string    `json:"version"`
+	ID              int64     `json:"id"` // either bolt auto id or unixnano //think nano is better cuz can check for dupery
+	Name            string    `json:"name"`
+	Lat             float64   `json:"lat"`
+	Lng             float64   `json:"long"`
+	Accuracy        float64   `json:"accuracy"`       // horizontal, in meters
+	VAccuracy       float64   `json:"vAccuracy"`      // vertical, in meteres
+	Elevation       float64   `json:"elevation"`      // in meters
+	Speed           float64   `json:"speed"`          // in kilometers per hour
+	SpeedAccuracy   float64   `json:"speed_accuracy"` // in meters per second
+	Tilt            float64   `json:"tilt"`           // degrees?
+	Heading         float64   `json:"heading"`        // in degrees
+	HeadingAccuracy float64   `json:"heading_accuracy"`
+	HeartRate       float64   `json:"heartrate"` // bpm
+	Time            time.Time `json:"time"`
+	Floor           int       `json:"floor"` // building floor if available
+	Notes           string    `json:"notes"` // special events of the day
+	COVerified      bool      `json:"COVerified"`
+	RemoteAddr      string    `json:"remoteaddr"`
+}
+
+func (p *TrackPoint) String() string {
+	return p.Name + " " + p.Time.Round(time.Second).String()
 }
 
 // Vector deepens
@@ -43,7 +49,7 @@ func (p *TrackPoint) Scale(val float64) simpleline.Point {
 	return &np
 }
 
-//Subtract add depee
+// Subtract add depee
 func (p *TrackPoint) Subtract(p2 simpleline.Point) simpleline.Point {
 	p2Cast := p2.(*TrackPoint)
 	np := TrackPoint{}
@@ -57,10 +63,10 @@ func (p *TrackPoint) Zero() simpleline.Point {
 	return &np
 }
 
-//TrackPoints is plural. might implement Len method for Sortabliilty
+// TrackPoints is plural. might implement Len method for Sortabliilty
 type TrackPoints []*TrackPoint
 
-//TPs has comm
+// TPs has comm
 type TPs []*TrackPoint
 
 func (tps TrackPoints) Verified() {
@@ -82,7 +88,7 @@ func (slice TPs) Len() int {
 	return len(slice)
 }
 func (slice TPs) Less(i, j int) bool {
-	return slice[i].Time.UnixNano() > slice[j].Time.UnixNano() //ids autoincrement //but time is better because it doesn't depend on the order of the array received from the request to bolty
+	return slice[i].Time.UnixNano() > slice[j].Time.UnixNano() // ids autoincrement //but time is better because it doesn't depend on the order of the array received from the request to bolty
 }
 func (slice TPs) Swap(i, j int) {
 	slice[i], slice[j] = slice[j], slice[i]
@@ -94,7 +100,7 @@ func (slice TrackPoints) Len() int {
 	return len(slice)
 }
 func (slice TrackPoints) Less(i, j int) bool {
-	return slice[i].Time.UnixNano() > slice[j].Time.UnixNano() //ids autoincrement //but time is better because it doesn't depend on the order of the array received from the request to bolty
+	return slice[i].Time.UnixNano() > slice[j].Time.UnixNano() // ids autoincrement //but time is better because it doesn't depend on the order of the array received from the request to bolty
 }
 func (slice TrackPoints) Swap(i, j int) {
 	slice[i], slice[j] = slice[j], slice[i]
